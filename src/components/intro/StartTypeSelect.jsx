7656a5662e4f260fd85d2c34175fd60c35760d5b
minkyeongc <cmk7424@naver.com>
2026-05-02 15:53:30 +0900

    Login 화면

--- src/components/intro/StartTypeSelect.jsx
+++ src/components/intro/StartTypeSelect.jsx
@@ -6,29 +6,27 @@ import characterImg from '../../assets/Character_JIWOO.png';
 function StartTypeSelect({ onNext }) {
   return (
     <div className="intro-page-wrapper">
-
       <img
-        src={characterImg} 
+        src={characterImg}
         alt="캐릭터"
         className="intro-character-img"
         style={{ left: '474px', top: '392px', width: '366px', height: '428px' }}
       />
 
-      <button 
-        className="intro_select-button" 
+      <button
+        className="intro_select-button"
         style={{ left: '1073px', top: '517px', width: '355px', height: '89px', backgroundColor: '#FBF3C4', borderColor: '#F4E695' }}
-        onClick={() => onNext({ startType: "newbie" })}
+        onClick={() => onNext({ startType: 'newbie' })}
       >
-        <span className="intro-button-text">처음 사용해요.</span>
+        <span className="intro-button-text">처음 사용해요</span>
       </button>
 
-
-      <button 
-        className="intro_select-button" 
+      <button
+        className="intro_select-button"
         style={{ left: '1073px', top: '641px', width: '355px', height: '89px', backgroundColor: '#E8FABF', borderColor: '#CDEF7E' }}
-        onClick={() => onNext({ startType: "usedBefore" })}
+        onClick={() => onNext({ startType: 'usedBefore' })}
       >
-        <span className="intro-button-text">사용해 봤어요.</span>
+        <span className="intro-button-text">사용해 봤어요</span>
       </button>
     </div>
   );
